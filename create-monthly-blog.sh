3f48612c928965974a42a1ea2c127f56bf9a23c8
Richard Littauer <richard+github@burntfen.com>
2018-05-17 11:28:50 -0400

    Slight edits for monthly version

--- create-monthly-blog.sh
+++ create-monthly-blog.sh
@@ -1,4 +1,14 @@
 #! /bin/sh
+#
+# To run: sh create-monthly-blog.sh june
+# Note: casing for june is odd. Maybe it should be lowercase.
+
+function createRepo () {
+  # Clean up the git
+  rm -rf .git
+  git init
+  hub create
+}
 
 # ./create-weekly-journal.sh 13
 function create () {
@@ -7,35 +17,40 @@ function create () {
 
   #statements
   SRC=/Users/richard/src
-  SOURCE='2018-week-12'
-  TARGET='2018-week-'$1
-  SOURCE_TITLE="2018 Week 12"
-  TARGET_TITLE="2018 Week "$1
+  SOURCE='2018-month'
+  TARGET='2018-'$1
+  SOURCE_TITLE="2018 Month"
+  TARGET_TITLE="2018 "$1" Blog"
 
   # Copy over the files and go into the dir
-  cp -r $SRC'/'$SOURCE $SRC'/'$TARGET
+  cp -r $SRC'/monthly-blog' $SRC'/'$TARGET
   cd $SRC'/'$TARGET
 
-  # Clean up the git
-  rm -rf .git
-  git init
-  hub create
+  createRepo
 
   # Clear all of the past content
   rm -rf _site _drafts/* _posts/*
-  # Replace 2018-week-12 string
+  # Replace 2018-month string
   ag -0 -l $SOURCE | xargs -0 sed -i '' "s/$SOURCE/$TARGET/g"
-  # Replace title in config.site and in README.md
+  # Replace title in README.md
   sed -i '' '1s/.*/# '$TARGET_TITLE'/' README.md
   ## The following ag rule breaks on spaces
   # ag -0 -l $SOURCE_TITLE | xargs -0 sed -i '' "s/$SOURCE/$TARGET_TITLE/g"
   # Replace description in config.site
+  # These don't work, either
+  echo 'Having some issues with setting yaml config, too'
   yaml set _config.yml site.title $TARGET_TITLE
-  yaml set _config.yml site.description "A journaling experiment for week 13 of 2018"
+  yaml set _config.yml site.description "A journaling experiment for the month of "$1" in 2018"
   echo 'Having some issues with `gh-description`'
+  echo 'Set them manually.'
   # gh-description RichardLitt/$TARGET "A journaling experiment for week '$1' of 2018"
 
+  echo 'Setting up a symlink to docs'
+  ln -s ~/src/$1/ /Users/richard/docs/$1
+
   echo 'Time to push!'
+  # Go to site
+  git browse "RichardLitt/"$TARGET
 
 }
 
